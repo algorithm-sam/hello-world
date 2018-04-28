b72170704489d29786e008f1cfb4f35d88f3c9f7
Algorithm-Sam <oluwatobisamuel2020@gmail.com>
2018-04-28 23:22:16 +0000

    Added new Features

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require ('express');
 const fs = require ('fs');
-
+const hbs = require('hbs');
 const port =process.env.PORT || 3000;
 const app = express();
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+});
+hbs.registerPartials(__dirname+'/views/partials');
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname+'/public'));
@@ -15,25 +19,22 @@ app.use((req,res,next) => {
     next();
 })
 
-app.use((req,res,next) => {
-    res.render('maintainance');
-})
+// app.use((req,res,next) => {
+//     res.render('maintainance');
+// })
 app.get('/',(req,res)=>{
     res.render('index.hbs',{
-        pageTitle: 'index',
-        date: new Date().getFullYear()
+        pageTitle: 'index'
     });
 });
 
 app.get('/about',(req,res)=>{
-    res.send({
-        page: 'about',
-        url: req.url,
-        method:req.method
-    });
+    res.render('about');
 });
 
-
+app.get('/projects',(req,res) => {
+    res.render('projects');
+})
 app.listen(port,()=>{
     console.log(`App running on port ${port}`);
 });
